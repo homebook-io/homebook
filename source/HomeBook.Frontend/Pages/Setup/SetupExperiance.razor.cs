7feb0f2456f66ac318d413df5b8bf44b4328ff64
lk-code <1656571+lk-code@users.noreply.github.com>
2025-08-25 12:24:45 +0200

    HB-17: add setup finish animation

--- source/HomeBook.Frontend/Pages/Setup/SetupExperiance.razor.cs
+++ source/HomeBook.Frontend/Pages/Setup/SetupExperiance.razor.cs
@@ -6,6 +6,9 @@ public partial class SetupExperiance : ComponentBase
 {
     private string _appVersion = string.Empty;
     private string _appServer = string.Empty;
+    private string _setupTiledBackgroundClass = "";
+    private string _setupContainerClass = "";
+    private string _setupBackgroundClass = "";
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
@@ -17,5 +20,27 @@ public partial class SetupExperiance : ComponentBase
         _appVersion = Configuration["Version"] ?? "Unknown";
         _appServer = Configuration["Backend:Host"] ?? "Unknown";
         StateHasChanged();
+    }
+
+    private async Task FinishSetupAnimationAsync()
+    {
+        CancellationToken cancellationToken = CancellationToken.None;
+
+        _setupContainerClass = "is-finished";
+        StateHasChanged();
+
+        await Task.Delay(1000, cancellationToken);
+
+        _setupTiledBackgroundClass = "is-finished";
+        StateHasChanged();
+
+        await Task.Delay(1000, cancellationToken);
+
+        _setupBackgroundClass = "is-finished";
+        StateHasChanged();
+
+        await Task.Delay(1000, cancellationToken);
+
+        // TODO: navigate to homebook start page
     }
 }
