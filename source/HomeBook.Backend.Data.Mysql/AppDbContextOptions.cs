925d4f6a586ef8046dca3784524119dc5cd79e8c
lk-code <1656571+lk-code@users.noreply.github.com>
2025-08-27 23:49:21 +0200

    HB-17: fix migrations schema

--- source/HomeBook.Backend.Data.Mysql/AppDbContextOptions.cs
+++ source/HomeBook.Backend.Data.Mysql/AppDbContextOptions.cs
@@ -4,14 +4,13 @@ namespace HomeBook.Backend.Data.Mysql;
 
 public static class AppDbContextOptions
 {
-    public const string HistoryTableName = "__EFMigrationsHistory";
-    public const string HistoryTableSchema = "hb";
+    private const string HISTORY_TABLE_NAME = "__EFMigrationsHistory";
 
     public static void SetDbOptions(this DbContextOptionsBuilder optionsBuilder, string connectionString)
     {
         string migrationAssembly = typeof(AppDbContext).Namespace ?? string.Empty;
         optionsBuilder.UseMySQL(connectionString,
-            x => x.MigrationsHistoryTable(HistoryTableName, HistoryTableSchema)
+            x => x.MigrationsHistoryTable(HISTORY_TABLE_NAME)
                 .EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)
                 .MigrationsAssembly(migrationAssembly)
         );
