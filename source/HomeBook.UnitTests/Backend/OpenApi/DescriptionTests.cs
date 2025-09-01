5ea4a5e09c26353ead960f08febd423c9de0c3bf
lk-code <1656571+lk-code@users.noreply.github.com>
2025-09-01 21:59:13 +0200

    HB-17: add completing tests for DescriptionBuilder-Class

--- source/HomeBook.UnitTests/Backend/OpenApi/DescriptionTests.cs
+++ source/HomeBook.UnitTests/Backend/OpenApi/DescriptionTests.cs
@@ -23,4 +23,36 @@ public class DescriptionTests
                         + "HTTP 400: this is a bad request response" + Environment.NewLine + Environment.NewLine
                         + "HTTP 500: this is an internal server error response");
     }
+    [Test]
+    public void Description_WithMultipleLines_Returns_WithStringAssignment()
+    {
+        // Arrange
+        var description = new Description("this is a description",
+            "HTTP 200: this is a success response",
+            "HTTP 400: this is a bad request response",
+            "HTTP 500: this is an internal server error response");
+
+        // Act
+        string result = description;
+
+        // Assert
+        result.ShouldBe("this is a description" + Environment.NewLine + Environment.NewLine
+                        + "HTTP 200: this is a success response" + Environment.NewLine + Environment.NewLine
+                        + "HTTP 400: this is a bad request response" + Environment.NewLine + Environment.NewLine
+                        + "HTTP 500: this is an internal server error response");
+    }
+
+    [Test]
+    public void Description_WithNoContent_Returns()
+    {
+        // Arrange
+        string[] lines = [];
+        var description = new Description(lines);
+
+        // Act
+        var result = description.ToString();
+
+        // Assert
+        result.ShouldBe("");
+    }
 }
