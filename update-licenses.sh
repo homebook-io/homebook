03828624b17a62ff5d71262efc766f4123a79a97
lk-code <1656571+lk-code@users.noreply.github.com>
2025-09-12 13:04:04 +0200

    HB-20: update licenses

---
+++ update-licenses.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Configuration variables
+SOLUTION="./homebook.slnx"
+LICENSES_JSON="./source/HomeBook.Backend.Core.Licenses/Licenses.json"
+LICENSES_DIR="./source/HomeBook.Backend.Core.Licenses/Licenses"
+
+dotnet tool install --global nuget-license
+dotnet restore
+dotnet build --configuration Release --no-restore
+
+rm "${LICENSES_JSON}"
+rm -rf "${LICENSES_DIR}"
+mkdir -p "${LICENSES_DIR}"
+
+nuget-license -i ${SOLUTION} --include-transitive --output JsonPretty --file-output ${LICENSES_JSON}
+nuget-license -i ${SOLUTION} --include-transitive -d ${LICENSES_DIR}
+
+echo "License information updated successfully."
