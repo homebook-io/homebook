041b9fe05dbcd503f0fd322781f9b22cc577a6f4
lk-code <1656571+lk-code@users.noreply.github.com>
2025-10-17 23:19:24 +0200

    HB-115: color changes

--- source/HomeBook.Frontend/Pages/Settings/Developer/Colors.razor.cs
+++ source/HomeBook.Frontend/Pages/Settings/Developer/Colors.razor.cs
@@ -13,24 +13,9 @@ public partial class Colors : ComponentBase
     private readonly List<string> _availableColors =
     [
         // modern colors
-        "amethyst", "cerulean", "crimson", "ruby",
-        "emerald", "amber", "teal", "petrol", "azure", "lavender",
-
-
-        // demo colors
-        // warme natürliche Töne
-        "sand", "beige", "linen", "amber1", "honey", "caramel", "clay", "terracotta", "copper", "bronze",
-
-        // erdige / organische Töne
-        "moss", "olive", "sage", "avocado", "forest", "basil", "fern", "khaki", "stone", "graphite",
-
-        // frische und aquatische Töne
-        "sea", "ocean", "aqua", "turquoise", "mint", "sky", "iceblue", "denim",
-
-        // elegante dunkle und neutrale Töne
-        "charcoal", "slate", "storm", "ash", "smoke", "shadow", "steel", "pebble", "fog", "cloud",
-
-        // moderne Akzentfarben
-        "plum", "rose", "coral", "peach", "apricot", "sunset", "wine", "mulberry", "magenta"
+        "amber", "amethyst", "apple", "aqua", "azure", "caramel", "cerulean", "charcoal", "chartreuse", "coral", "crimson",
+        "denim", "emerald", "fern", "gold", "graphite", "honey", "jade", "lavender", "lemon", "mulberry", "peach",
+        "petrol", "plum", "rose", "ruby", "shadow", "smoke", "spring", "steel", "stone", "storm", "sunset", "teal",
+        "wine", "ocean"
     ];
 }
