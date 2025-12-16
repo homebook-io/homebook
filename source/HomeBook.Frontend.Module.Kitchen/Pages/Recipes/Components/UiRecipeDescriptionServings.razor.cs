74223a4fd6aa7a1203499bc6a74b60f9a3f67774
lk-code <1656571+lk-code@users.noreply.github.com>
2025-12-16 23:34:07 +0100

    HB-135 - Makes recipe servings editable
    Exposes recipe description and servings as parameters.
    
    This allows the recipe servings to be dynamically set.

--- source/HomeBook.Frontend.Module.Kitchen/Pages/Recipes/Components/UiRecipeDescriptionServings.razor.cs
+++ source/HomeBook.Frontend.Module.Kitchen/Pages/Recipes/Components/UiRecipeDescriptionServings.razor.cs
@@ -4,4 +4,9 @@ namespace HomeBook.Frontend.Module.Kitchen.Pages.Recipes.Components;
 
 public partial class UiRecipeDescriptionServings : ComponentBase
 {
+    [Parameter]
+    public string Description { get; set; } = string.Empty;
+
+    [Parameter]
+    public int Servings { get; set; } = 1;
 }
