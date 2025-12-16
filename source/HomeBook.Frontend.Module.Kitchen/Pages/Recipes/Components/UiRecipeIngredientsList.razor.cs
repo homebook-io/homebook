7b36a0b7ef1dee16b29313a2b375d8288c8b0249
lk-code <1656571+lk-code@users.noreply.github.com>
2025-12-16 23:36:03 +0100

    HB-135 - Makes ingredients and steps parameters
    Refactors the UiRecipeIngredientsList and UiRecipeStepsList components to accept ingredients and steps as parameters.
    
    This change enhances the reusability and testability of these components by decoupling them from a specific recipe instance.

--- source/HomeBook.Frontend.Module.Kitchen/Pages/Recipes/Components/UiRecipeIngredientsList.razor.cs
+++ source/HomeBook.Frontend.Module.Kitchen/Pages/Recipes/Components/UiRecipeIngredientsList.razor.cs
@@ -1,7 +1,10 @@
+using HomeBook.Frontend.Module.Kitchen.ViewModels;
 using Microsoft.AspNetCore.Components;
 
 namespace HomeBook.Frontend.Module.Kitchen.Pages.Recipes.Components;
 
 public partial class UiRecipeIngredientsList : ComponentBase
 {
+    [Parameter]
+    public IEnumerable<IngredientViewModel> Ingredients { get; set; } = new List<IngredientViewModel>();
 }
--- source/HomeBook.Frontend.Module.Kitchen/Pages/Recipes/Components/UiRecipeStepsList.razor.cs
+++ source/HomeBook.Frontend.Module.Kitchen/Pages/Recipes/Components/UiRecipeStepsList.razor.cs
@@ -1,7 +1,10 @@
+using HomeBook.Frontend.Module.Kitchen.ViewModels;
 using Microsoft.AspNetCore.Components;
 
 namespace HomeBook.Frontend.Module.Kitchen.Pages.Recipes.Components;
 
 public partial class UiRecipeStepsList : ComponentBase
 {
+    [Parameter]
+    public IEnumerable<StepViewModel> Steps { get; set; } = new List<StepViewModel>();
 }
