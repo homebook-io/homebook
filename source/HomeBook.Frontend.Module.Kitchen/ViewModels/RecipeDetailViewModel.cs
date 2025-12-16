a2cbe4be02b6c4fe30205d196a1eabb402949251
lk-code <1656571+lk-code@users.noreply.github.com>
2025-12-16 23:26:04 +0100

    HB-135 - Implements recipe edit components
    Adds several new components for editing recipe details,
    including description, servings, ingredients, steps,
    image upload, and meta information like duration and source.
    
    These components provide a structured UI for managing
    recipe data.

--- source/HomeBook.Frontend.Module.Kitchen/ViewModels/RecipeDetailViewModel.cs
+++ source/HomeBook.Frontend.Module.Kitchen/ViewModels/RecipeDetailViewModel.cs
@@ -9,9 +9,14 @@ public class RecipeDetailViewModel
     public int? Servings { get; set; }
     public int? CaloriesKcal { get; set; }
     public TimeSpan? Duration { get; set; }
+    public TimeSpan? DurationWorkingMinutes { get; set; }
+    public TimeSpan? DurationCookingMinutes { get; set; }
+    public TimeSpan? DurationRestingMinutes { get; set; }
     public IEnumerable<IngredientViewModel> Ingredients { get; set; }
     public IEnumerable<StepViewModel> Steps { get; set; }
     public string Image { get; set; }
+    public string Source { get; set; }
+    public string Comment { get; set; }
 
     public bool HasAnnotations => Duration.HasValue || Servings.HasValue;
 }
