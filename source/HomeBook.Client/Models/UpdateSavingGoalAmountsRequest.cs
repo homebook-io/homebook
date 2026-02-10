47a6c6adb5a9855eaee7dd1d22300910a75232d1
lk-code <1656571+lk-code@users.noreply.github.com>
2026-02-10 16:12:45 +0100

    HB-95 - type: refactor: update client models
    Updates Kiota client models to support new interest rate options
    Refactors CreateSavingGoalRequest and UpdateSavingGoalAmountsRequest models
    Adds strict JSON number handling in backend configuration
    Updates OpenAPI to version 3.0
    Upgrades Kiota version to 1.30.0

--- source/HomeBook.Client/Models/UpdateSavingGoalAmountsRequest.cs
+++ source/HomeBook.Client/Models/UpdateSavingGoalAmountsRequest.cs
@@ -19,7 +19,13 @@ namespace HomeBook.Client.Models
         /// <summary>The interestRate property</summary>
         public double? InterestRate { get; set; }
         /// <summary>The interestRateOption property</summary>
-        public int? InterestRateOption { get; set; }
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption? InterestRateOption { get; set; }
+#nullable restore
+#else
+        public global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption InterestRateOption { get; set; }
+#endif
         /// <summary>The monthlyPayment property</summary>
         public double? MonthlyPayment { get; set; }
         /// <summary>The targetAmount property</summary>
@@ -51,7 +57,7 @@ namespace HomeBook.Client.Models
             {
                 { "currentAmount", n => { CurrentAmount = n.GetDoubleValue(); } },
                 { "interestRate", n => { InterestRate = n.GetDoubleValue(); } },
-                { "interestRateOption", n => { InterestRateOption = n.GetIntValue(); } },
+                { "interestRateOption", n => { InterestRateOption = n.GetObjectValue<global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption>(global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption.CreateFromDiscriminatorValue); } },
                 { "monthlyPayment", n => { MonthlyPayment = n.GetDoubleValue(); } },
                 { "targetAmount", n => { TargetAmount = n.GetDoubleValue(); } },
             };
@@ -65,10 +71,85 @@ namespace HomeBook.Client.Models
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
             writer.WriteDoubleValue("currentAmount", CurrentAmount);
             writer.WriteDoubleValue("interestRate", InterestRate);
-            writer.WriteIntValue("interestRateOption", InterestRateOption);
+            writer.WriteObjectValue<global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption>("interestRateOption", InterestRateOption);
             writer.WriteDoubleValue("monthlyPayment", MonthlyPayment);
             writer.WriteDoubleValue("targetAmount", TargetAmount);
             writer.WriteAdditionalData(AdditionalData);
+        }
+        /// <summary>
+        /// Composed type wrapper for classes <see cref="global::HomeBook.Client.Models.InterestRateOptions"/>, <see cref="global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest_interestRateOptionMember1"/>
+        /// </summary>
+        [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
+        public partial class UpdateSavingGoalAmountsRequest_interestRateOption : IComposedTypeWrapper, IParsable
+        {
+            /// <summary>Composed type representation for type <see cref="global::HomeBook.Client.Models.InterestRateOptions"/></summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+            public global::HomeBook.Client.Models.InterestRateOptions? InterestRateOptions { get; set; }
+#nullable restore
+#else
+            public global::HomeBook.Client.Models.InterestRateOptions InterestRateOptions { get; set; }
+#endif
+            /// <summary>Composed type representation for type <see cref="global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest_interestRateOptionMember1"/></summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+            public global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest_interestRateOptionMember1? UpdateSavingGoalAmountsRequestInterestRateOptionMember1 { get; set; }
+#nullable restore
+#else
+            public global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest_interestRateOptionMember1 UpdateSavingGoalAmountsRequestInterestRateOptionMember1 { get; set; }
+#endif
+            /// <summary>
+            /// Creates a new instance of the appropriate class based on discriminator value
+            /// </summary>
+            /// <returns>A <see cref="global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption"/></returns>
+            /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+            public static global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption CreateFromDiscriminatorValue(IParseNode parseNode)
+            {
+                if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
+                var mappingValue = parseNode.GetChildNode("")?.GetStringValue();
+                var result = new global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest.UpdateSavingGoalAmountsRequest_interestRateOption();
+                if("".Equals(mappingValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.InterestRateOptions = new global::HomeBook.Client.Models.InterestRateOptions();
+                }
+                else if("".Equals(mappingValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.UpdateSavingGoalAmountsRequestInterestRateOptionMember1 = new global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest_interestRateOptionMember1();
+                }
+                return result;
+            }
+            /// <summary>
+            /// The deserialization information for the current model
+            /// </summary>
+            /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
+            public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+            {
+                if(InterestRateOptions != null)
+                {
+                    return InterestRateOptions.GetFieldDeserializers();
+                }
+                else if(UpdateSavingGoalAmountsRequestInterestRateOptionMember1 != null)
+                {
+                    return UpdateSavingGoalAmountsRequestInterestRateOptionMember1.GetFieldDeserializers();
+                }
+                return new Dictionary<string, Action<IParseNode>>();
+            }
+            /// <summary>
+            /// Serializes information the current object
+            /// </summary>
+            /// <param name="writer">Serialization writer to use to serialize this model</param>
+            public virtual void Serialize(ISerializationWriter writer)
+            {
+                if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+                if(InterestRateOptions != null)
+                {
+                    writer.WriteObjectValue<global::HomeBook.Client.Models.InterestRateOptions>(null, InterestRateOptions);
+                }
+                else if(UpdateSavingGoalAmountsRequestInterestRateOptionMember1 != null)
+                {
+                    writer.WriteObjectValue<global::HomeBook.Client.Models.UpdateSavingGoalAmountsRequest_interestRateOptionMember1>(null, UpdateSavingGoalAmountsRequestInterestRateOptionMember1);
+                }
+            }
         }
     }
 }
